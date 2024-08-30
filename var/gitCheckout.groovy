846b96b77c2dc5721de5acaee14a000a02319311
reginbilnesar <reginbilnesar@gmail.com>
2024-08-30 12:06:58 +0530

    changeds

--- var/gitCheckout.groovy
+++ var/gitCheckout.groovy
@@ -2,9 +2,6 @@ def call(Map stageParams) {
     checkout([
         $class: 'GitSCM',
         branches: [[name: stageParams.branch]],
-        doGenerateSubmoduleConfigurations: false,
-        extensions: [],
-        submoduleCfg: [],
         userRemoteConfigs: [[url: stageParams.url]]
     ])
 }
